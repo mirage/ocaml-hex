8d78bfe13ad66013de4eb43a8a572abf53315839
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-10-24 21:10:03 +0200

    Test the new Travis CI script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,5 @@
-sudo add-apt-repository "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe"
-sudo add-apt-repository --yes ppa:avsm/ocaml42+opam12
-sudo apt-get update -qq
-sudo apt-get install -y ocaml-compiler-libs ocaml-interp ocaml-base-nox ocaml-base ocaml ocaml-nox ocaml-native-compilers camlp4 camlp4-extra opam
-
-export OPAMYES=1
-opam init -a
-opam pin add local-package . -n
-opam install local-package --deps-only --build-test
+wget https://raw.githubusercontent.com/samoht/ocaml-travisci-skeleton/master/.travis-opam.sh
+sh .travis-opam.sh
 
 eval `opam config env`
 ./configure --enable-tests
